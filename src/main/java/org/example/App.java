a6899acdcbae2c5b516504c1aec8c1b59bbf80b3
td_qinwei <qinwei@earthcoder.net>
2020-06-28 23:41:27 +0800

    按照单词频率排序。

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,40 +6,27 @@ public class App {
 
     public static final String CONTENT_PDF = "C:\\Users\\td_qinwei\\OneDrive\\Documents\\SD_BOOK\\技术_数据库\\Teradata Database 15.10\\Database Design.pdf";
 
-
-    public static void printMap(Map<String, Integer> map) {
-    /*
-    map = map.entrySet().stream().sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (oldValue, newValue) -> oldValue,
-                        LinkedHashMap::new));
-        */
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ":" + entry.getValue() + "," + containsCharacter(entry.getValue().toString()));
-        }
-    }
-
-    public static String[] splitString(String string) {
-        final Set<String> caseInsensitiveSet = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
-        caseInsensitiveSet.addAll(Arrays.asList(string.split(" ")));
-        return caseInsensitiveSet.toArray(new String[0]);
-    }
-
-    public static boolean containsCharacter(String string) {
-        return string.matches("\\w");
-    }
-
     public static void main( String[] args ) {
 
-        String original = PDFBoxReader.readText(CONTENT_PDF, 2);
-        final String[] strings = splitString(original);
         final Map<String, Integer> map = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-        for(String word: strings) {
-            map.put(word, map.containsKey(word) ? map.get(word) + 1 : 1);
+
+        for(String word: PDFBoxReader.readText(CONTENT_PDF).split(" ")) {
+            if (word.contains(",")) {
+                word = word.replaceAll(",", "");
+            } else if (word.contains("\n")) {
+                word = word.replaceAll("\n", "");
+            } else if (word.contains(".")) {
+                word = word.replaceAll(".", "");
+            }
+            if (word.length() > 2) {
+                map.put(word, map.containsKey(word) ? map.get(word) + 1 : 1);
+            }
+            if (word.contains("\n")) {
+                System.out.println("{[" + word + "],,,,,,[" + word.replaceAll("\n", "") + "]}");
+            }
         }
-        //String now = original.replaceAll("[^(A-Za-z)]", "");
-        System.out.println(original);
+
+        final List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
     }
 }
